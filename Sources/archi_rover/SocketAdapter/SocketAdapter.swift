a08a502b78f1abf6d0321bad28fc6c05868b83ce
hugo <hugobast33@gmail.com>
2023-11-10 17:05:35 +0100

    fix

--- Sources/archi_rover/SocketAdapter/SocketAdapter.swift
+++ Sources/archi_rover/SocketAdapter/SocketAdapter.swift
@@ -12,7 +12,6 @@ class SocketAdapter: ICommandReceiver, ICommandSender {
 
     init() {
         self.socketManager = SocketManager(host: "127.0.0.1", port: 12345)
-        NotificationCenter.default.addObserver(self, selector: #selector(getNotified(_:)), name: SocketManager.messageReceivedNotification, object: nil)
     }
 
     func sendCommand(_ command: String) {
